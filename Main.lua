5ebe4ecccd32c45e0454c5ae10f5e7f2f8c5c433
Muhammad6916 <124334344+Muhammad6916@users.noreply.github.com>
2023-02-15 16:24:59 +0800

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -1,2 +1,2 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Muhammad6196/Project-WD/main/Main.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Muhammad6196/Project-WD/main/Mainstring.lua"))()
 loadstring(game:HttpGet("https://raw.githubusercontent.com/PsxScriptMaker/SnowHub/main/Main.lua"))()
